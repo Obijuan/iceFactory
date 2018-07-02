723d57427f908fcb1f047ee2adf032e478c47756
obijuan <juan@iearobotics.com>
2018-07-02 15:00:24 +0200

    Web version: Comments updated

--- src/icetable_front.js
+++ src/icetable_front.js
@@ -19,29 +19,26 @@ function iceTable_gen() {
   console.log("Salidas: " + M)
 
 
-  //-- Generated the icestudio object for the NxM table
+  //------------ Generate the icestudio object for the NxM table
   obj = iceTable(N, M);
 
-  //--- Write the object into a file
+  //-------------- Generate the icestudio file
   const OUTPUT_FILENAME = "tabla-" + N + "-" + M + ".ice";
   console.log("File generated: " + OUTPUT_FILENAME)
 
+  //-- json object to plain ascii text
   let json = JSON.stringify(obj);
 
-  // creas el fichero con la API File
+  // Write a temporal file in the browser
   var file = new File([json],OUTPUT_FILENAME,{type:"text/plain;charset=utf-8"});
 
-  // obtienes una URL para el fichero que acabas de crear
+  // Get the URL of the temporal file created
   var url  = window.URL.createObjectURL(file);
 
-  console.log(url);
+  //console.log(url);
 
-  // creas un enlace y lo añades al documento
-  //var a = document.createElement("a");
-  //document.body.appendChild(a);
+  //-------------- Add the link to the icestudio file
   var a = document.getElementById("obj_file");
-
-  // actualizas los parámetros del enlace para descargar el fichero creado
   a.href = url;
   a.innerHTML = "Descargar fichero";
   a.download = file.name;
